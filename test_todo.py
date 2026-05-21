f7694880833087356e60eb3fe5fafeafb9f2ac6e
maksim.evlanov <maksim.evlanov@red-soft.ru>
2026-05-21 16:53:38 +0300

    conftest

---
+++ test_todo.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+@pytest.mark.only_browser("chromium")
+def test_add_todo(page):
+    page.goto("https://demo.playwright.dev/todomvc/#/")
+    page.get_by_placeholder("What needs to be done?").click()
+    page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий playwright")
+    page.get_by_placeholder("What needs to be done?").press("Enter")
